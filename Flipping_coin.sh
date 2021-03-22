ab9a6f491a38ba0ace66c0e7c40009848a970226
shuchitasingh <singhshuchita5@gmail.com>
2021-03-23 00:15:24 +0530

    Added Duble siglet no and percentage

--- Flipping_coin.sh
+++ Flipping_coin.sh
@@ -54,3 +54,75 @@ done
 echo "Singlet Combination is "${coin[@]}
 echo "Head percentage is $Headpercent%"
 echo "Tail percentage is $Tailpercent%"
+
+
+#FUNCTION TO CHECK DOUBLET COMBINATION
+function isFlip(){
+local NumberCoin=$1
+for (( flip=0; flip<$flipCoin; flip++ ))
+do
+   for (( coin=0; coin<$NumberCoin; coin++ ))
+   do
+      randomCheck=$((RANDOM%2))
+      if [ $randomCheck -eq $ISHEAD ]
+      then
+         echo "Head"
+         coinSide+=H
+      else
+         echo "Tails"
+         coinSide+=T
+      fi
+   done
+#STORING THE COMBINATION IN DICTIONARY
+   if [ $NumberCoin -eq $SINGLE ]
+   then
+      ((singleFlip[$coinSide]++))
+      echo ${!singleFlip[@]}
+   elif [ $NumberCoin -eq $DOUBLE ]
+   then
+      ((doubleFlip[$coinSide]++))
+      echo ${!doubleFlip[@]}
+   else
+      echo "Invalid input"
+   fi
+   coinSide=""
+done
+}
+echo "Enter 1 for Single Combination"
+echo "Enter 2 for Double Combination"
+
+#FUNCTION TO GET PERCENTAGE
+function getPercent(){
+local NumberCoin=$1
+if [ $NumberCoin -eq $SINGLE ]
+then
+   for index in ${!singleFlip[@]}
+   do
+      singleFlip[$index]=`echo "scale=2; ${singleFlip[$index]}*100/$flipCoin" | bc`
+      echo ${singleFlip[@]}
+   done
+elif [ $NumberCoin -eq $DOUBLE ]
+then
+   for index in ${!doubleFlip[@]}
+   do
+      doubleFlip[$index]=`echo "scale=2; ${doubleFlip[$index]}*100/$flipCoin" | bc`
+      echo ${doubleFlip[@]}
+   done
+else
+   echo "No such value"
+fi
+}
+
+#SWITCH CASE FOR CHOICES
+read -p "Enter your choice " choice
+case $choice in
+   $SINGLE)
+   isFlip $SINGLE
+   getPercent $SINGLE
+   ;;
+   $DOUBLE)
+   isFlip $DOUBLE
+   getPercent $DOUBLE
+   ;;
+*)
+esac
