d6e92776e4a7fc4334190a000d35e3461ce40031
shuchitasingh <singhshuchita5@gmail.com>
2021-03-23 00:06:19 +0530

    Added:Siglet combination in Dict

--- Flipping_coin.sh
+++ Flipping_coin.sh
@@ -28,3 +28,29 @@ break
 fi
 i=$(expr $i + 1 )
 done
+
+
+ISHEAD=1
+
+#USERINPUT
+declare -A coin
+read -p "Enter number of times to flip " number
+
+#RANDOM VALUE FOR HEADS AND TAIL
+for (( counter=0; counter<number; counter++ ))
+do
+	randomFlip=$((RANDOM%2))
+	if [ $randomFlip -eq $ISHEAD 
+	then
+		coin[$counter]=H
+		((headCount++))
+		Headpercent=`echo "scale=2; $headCount*100/$number" | bc`
+	else
+		coin[$counter]=T
+		((tailCount++))
+		Tailpercent=`echo "scale=2; $tailCount*100/$number" | bc`
+	fi
+done
+echo "Singlet Combination is "${coin[@]}
+echo "Head percentage is $Headpercent%"
+echo "Tail percentage is $Tailpercent%"
